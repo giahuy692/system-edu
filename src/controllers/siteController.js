cf6428668df4f0c8b1a2c59b04dbbf6a10656781
tuando57 <phantuandont85@gmail.com>
2022-06-21 23:32:21 +0700

    intro duuuuu

--- src/controllers/siteController.js
+++ src/controllers/siteController.js
@@ -2,6 +2,9 @@ class SiteController {
     home(req, res){
         res.send('Hello world');
     }
+    introdu(req, res){
+        res.send('Hello  introdu');
+    }
 }
 
 module.exports = new SiteController
--- src/routes/site.js
+++ src/routes/site.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const siteController = require('../controllers/SiteController');
 
+router.get('/introdu', siteController.introduu) ;
 router.get('/', siteController.home) ;
+
 
 module.exports = router;
